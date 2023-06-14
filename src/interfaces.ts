1f2b88693b7fcad349570ed6b892af065890c464
MelnykovSV <svmelnykov@gmail.com>
2023-06-14 19:31:10 +0300

    1

---
+++ src/interfaces.ts
@@ -0,0 +1,10 @@
+export interface ISignUpData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface ISignInData {
+  email: string;
+  password: string;
+}
---
+++ src/redux/auth/operations.ts
@@ -0,0 +1,79 @@
+import axios, { AxiosError } from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { getErrorMessage } from '../../getErrorMessage';
+// import toast from 'react-toastify';
+import { ISignUpData, ISignInData } from '../../interfaces';
+
+axios.defaults.baseURL = 'https://bookread-backend.goit.global';
+
+const token = {
+  set(token: string) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
+};
+
+export const signUp = createAsyncThunk(
+  'auth/signUp',
+  async ({ name, email, password }: ISignUpData, thunkAPI) => {
+    try {
+      const response = axios.post('/auth/register', { name, email, password });
+      console.log(response);
+    } catch (error) {
+      console.log(thunkAPI.rejectWithValue(getErrorMessage(error)));
+      return thunkAPI.rejectWithValue(getErrorMessage(error));
+    }
+  }
+);
+
+export const signIn = createAsyncThunk(
+  'auth/signIn',
+  async ({ email, password }: ISignInData, thunkAPI) => {
+    try {
+      const response = axios.post('auth/login', { email, password });
+      console.log(response);
+    } catch (error) {
+      console.log(thunkAPI.rejectWithValue(getErrorMessage(error)));
+      return thunkAPI.rejectWithValue(getErrorMessage(error));
+    }
+  }
+);
+
+export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
+  try {
+    const response = axios.post('auth/logout');
+    console.log(response);
+  } catch (error) {
+    console.log(thunkAPI.rejectWithValue(getErrorMessage(error)));
+    return thunkAPI.rejectWithValue(getErrorMessage(error));
+  }
+});
+
+export const refresh = createAsyncThunk(
+  'auth/refresh',
+  async (sid, thunkAPI) => {
+    try {
+      const response = axios.post('auth/refresh', { sid });
+      console.log(response);
+    } catch (error) {
+      console.log(thunkAPI.rejectWithValue(getErrorMessage(error)));
+      return thunkAPI.rejectWithValue(getErrorMessage(error));
+    }
+  }
+);
+
+export const getUserData = createAsyncThunk(
+  'auth/getUserData',
+  async (sid, thunkAPI) => {
+    try {
+      const response = axios.get('user/books');
+      console.log(response);
+    } catch (error) {
+      console.log(thunkAPI.rejectWithValue(getErrorMessage(error)));
+      return thunkAPI.rejectWithValue(getErrorMessage(error));
+    }
+  }
+);
