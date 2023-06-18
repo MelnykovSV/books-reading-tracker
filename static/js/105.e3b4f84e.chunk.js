b77c02b6eae5ccf9619c0157072cdd1c5709d6dc
MelnykovSV <114297826+MelnykovSV@users.noreply.github.com>
2023-06-18 14:54:17 +0000

    Deploying to gh-pages from @ MelnykovSV/books-reading-tracker@7bc7e5f6dfa8a18e7da3e22fbab1f1eac75236a0 🚀

---
+++ static/js/105.e3b4f84e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbooks_reading_tracker=self.webpackChunkbooks_reading_tracker||[]).push([[105],{8105:function(n,r,e){e.r(r),e.d(r,{default:function(){return c}});var a,i=e(168),t=e(9202).Z.div(a||(a=(0,i.Z)([""]))),u=e(184),c=function(){return(0,u.jsx)(t,{children:"TrainingPage"})}}}]);
+//# sourceMappingURL=105.e3b4f84e.chunk.js.map
