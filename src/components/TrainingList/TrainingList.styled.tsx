79c0229494d8440875cfdc29c6b80794a503aa88
MelnykovSV <svmelnykov@gmail.com>
2023-07-14 20:09:23 +0300

    tables styled

--- src/components/TrainingList/TrainingList.styled.tsx
+++ src/components/TrainingList/TrainingList.styled.tsx
@@ -24,14 +24,151 @@ export const Container = styled.div<IStyledProps>`
     padding-left: 0;
     padding-top: 11px;
     padding-bottom: 11px;
+    padding: 0;
     line-height: normal;
   }
   .MuiTableRow-root:first-of-type .MuiTableCell-body {
-    padding-top: 20px;
+    /* padding-top: 20px; */
   }
-  .MuiTableCell-body:first-of-type {
+
+  .MuiTableCell-body:not(:last-of-type) {
+    padding-right: 30px;
+  }
+  .MuiTableCell-body:last-of-type {
+    padding-right: 10px;
+  }
+  .MuiTableRow-root {
+    border-bottom: 1 solid red;
+  }
+  .title-container {
     display: flex;
     align-items: center;
-    gap: 21px;
+    gap: 14px;
+
+    max-width: 370px;
+
+    @media screen and (min-width: 1280px) {
+      max-width: 470px;
+    }
+
+    min-width: 0;
+    p {
+      min-width: 0;
+      @media screen and (min-width: 768px) and (max-width: 1279px) {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+
+        @supports (-webkit-line-clamp: 2) {
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: initial;
+          display: -webkit-box;
+          -webkit-line-clamp: 2;
+          -webkit-box-orient: vertical;
+        }
+      }
+
+      @media screen and (min-width: 1280px) {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+    }
+
+    svg {
+      min-width: 22px;
+    }
+  }
+
+  .author-container {
+    max-width: 130px;
+
+    @media screen and (min-width: 1280px) {
+      max-width: 170px;
+    }
+
+    /* min-width: 0; */
+
+    p {
+      min-width: 0;
+      @media screen and (min-width: 768px) and (max-width: 1279px) {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+
+        @supports (-webkit-line-clamp: 2) {
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: initial;
+          display: -webkit-box;
+          -webkit-line-clamp: 2;
+          -webkit-box-orient: vertical;
+        }
+      }
+
+      @media screen and (min-width: 1280px) {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+    }
+  }
+  .last-row {
+    height: 62px;
+    display: block;
+    @media screen and (min-width: 1280px) {
+      height: 37px;
+    }
+    .title-container {
+      padding-top: 22px;
+      padding-bottom: 23px;
+      @media screen and (min-width: 1280px) {
+        padding-top: 10px;
+        padding-bottom: 11px;
+      }
+    }
+  }
+
+  .book-icon {
+    fill: #a6abb9;
+  }
+  .delete-button {
+    position: absolute;
+    top: 20px;
+    right: 7px;
+
+    cursor: pointer;
+
+    width: 32px;
+    height: 32px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    border: none;
+    background-color: #f6f7fb;
+    transition: background-color 0.3s linear;
+    border-radius: 50%;
+
+    svg {
+      display: block;
+      transition: fill 0.3s linear;
+      fill: #a6abb9;
+    }
+    &:hover,
+    &:focus {
+      background-color: #a6abb9;
+      svg {
+        fill: white;
+      }
+    }
+
+    @media screen and (min-width: 768px) {
+      position: static;
+      grid-column: 5;
+      justify-self: right;
+    }
   }
 `;
--- src/components/TrainingList/TrainingList.tsx
+++ src/components/TrainingList/TrainingList.tsx
@@ -8,6 +8,8 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { ITrainingList } from '../../interfaces';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import { ReactComponent as BookIcon } from '../../images/icons/library.svg';
+import { ReactComponent as DeleteIcon } from '../../images/icons/delete.svg';
 
 export const TrainingList = ({
   trainingList,
@@ -25,22 +27,21 @@ export const TrainingList = ({
           borderTop: 1,
           borderBottom: 1,
           borderRadius: 0,
-          maxHeight: matches ? 212 : 227,
+
+          height: matches ? 195 : 230,
           borderColor: '#E0E5EB',
           boxShadow: 'none',
           '&::-webkit-scrollbar': {
             width: 5,
-            // height: 100,
           },
           '& MuiTableRow-head': {
             backgroundColor: '#B1B5C2',
-            // height: 100,
           },
           '&::-webkit-scrollbar-track': {
             backgroundColor: 'rgba(177, 181, 194, 0.2)',
             width: 10,
 
-            marginTop: 7,
+            marginTop: 5,
           },
           '&::-webkit-scrollbar-thumb': {
             backgroundColor: '#B1B5C2',
@@ -59,9 +60,15 @@ export const TrainingList = ({
         >
           <TableHead sx={{ color: 'white' }}>
             <TableRow>
-              <TableCell align="left">Book title</TableCell>
-              <TableCell align="left">Author</TableCell>
-              <TableCell align="left">Year</TableCell>
+              <TableCell width={matches ? 500 : 400} align="left">
+                Book title
+              </TableCell>
+              <TableCell width={matches ? 200 : 160} align="left">
+                Author
+              </TableCell>
+              <TableCell width={matches ? 100 : 92} align="left">
+                Year
+              </TableCell>
               <TableCell align="left">Pages</TableCell>
               <TableCell align="left"></TableCell>
             </TableRow>
@@ -70,16 +77,23 @@ export const TrainingList = ({
             {trainingList.map(row => (
               <TableRow
                 key={row._id}
-                // sx={{ '&:last-of-type td, &:last-of-type th': { border: 0 } }}
                 sx={{
+                  height: matches ? 37 : 62,
                   '&:last-of-type td, &:last-of-type th': { border: 0 },
                   [`& .${tableCellClasses.root}`]: {
-                    borderBottom: 'none',
+                    borderBottom: matches ? 'none' : 1,
+                    borderColor: '#E0E5EB',
                   },
                 }}
               >
-                <TableCell align="left">{row.title}</TableCell>
-                <TableCell align="left">{row.author}</TableCell>
+                <TableCell align="left">
+                  <div className="title-container">
+                    <BookIcon className="book-icon" /> <p>{row.title}</p>
+                  </div>
+                </TableCell>
+                <TableCell align="left" className="author-container">
+                  <p>{row.author}</p>
+                </TableCell>
                 <TableCell align="left">{row.publishYear}</TableCell>
                 <TableCell align="left">{row.pagesTotal}</TableCell>
                 <TableCell align="left">
@@ -88,12 +102,33 @@ export const TrainingList = ({
                     onClick={() => {
                       removeFromTrainingListHandler(row._id);
                     }}
+                    className="delete-button"
                   >
-                    D
+                    <DeleteIcon className="delete-icon" />
                   </button>
                 </TableCell>
               </TableRow>
             ))}
+            <TableRow
+              className="last-row"
+              sx={{
+                '&:last-of-type td, &:last-of-type th': { border: 0 },
+                [`& .${tableCellClasses.root}`]: {
+                  borderBottom: 'none',
+                },
+              }}
+            >
+              <TableCell align="left">
+                {' '}
+                <div className="title-container">
+                  <BookIcon className="book-icon" /> ...
+                </div>
+              </TableCell>
+              <TableCell align="left"></TableCell>
+              <TableCell align="left"></TableCell>
+              <TableCell align="left"></TableCell>
+              <TableCell align="left"></TableCell>
+            </TableRow>
           </TableBody>
         </Table>
       </TableContainer>
